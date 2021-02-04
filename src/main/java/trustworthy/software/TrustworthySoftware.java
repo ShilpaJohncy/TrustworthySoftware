c51799634ea3b912d694810d3d7c861a2a529a27
Shilpa <shilpamaria.johncy@gmail.com>
2021-02-04 12:15:37 +0000

    Added the other apps to possibly test

--- src/main/java/trustworthy/software/TrustworthySoftware.java
+++ src/main/java/trustworthy/software/TrustworthySoftware.java
@@ -6,7 +6,10 @@ import static trustworthy.software.security.CVSSTest.*;
 public class TrustworthySoftware {
     public static void main(String[] args) {
         extractCVSSScore("Zoom", "zoom", "");
-//        extractCVSSScore("Microsoft", "Teams", "");
+        extractCVSSScore("Microsoft", "word", "");
         extractCVSSScore("notepad-plus-plus", "Notepad\\+\\+", "7.6.6");
+        extractCVSSScore("adobe", "acrobat_reader_dc", "20.012.20048");
+        extractCVSSScore("mcafee", "livesafe", "");
+        extractCVSSScore("google", "chrome", "");
     }
 }
