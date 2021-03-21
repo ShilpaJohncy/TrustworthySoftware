00847261ebb9add347ab6822ca4e5d9e6ec35e37
Shilpa <shilpamaria.johncy@gmail.com>
2021-03-21 01:03:11 +0000

    Added functionality for restricting the weightage to a total of 100. Also implemented hover description for each field.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,23 +1,44 @@
 import React from "react";
-import {faFolder} from "@fortawesome/free-solid-svg-icons";
+import {faFolder, faQuestionCircle} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {Popup} from "semantic-ui-react"
+
+let total = 0;
+
+function findTotal() {
+    let tot = 0;
+    let arr = document.getElementsByName('percentage');
+    for (let i = 0; i < arr.length; i++) {
+        if (parseInt(arr[i].value)) {
+            tot += parseInt(arr[i].value);
+            if (tot > 100) {
+                alert("Please make sure numbers total 100.");
+                arr[i].value = null;
+                return false;
+            }
+            total = tot;
+        }
+    }
+}
+
+function ifSmallerSum() {
+    let arr = document.getElementsByName('percentage');
+    if (total < 100 && (arr[4].value > 0)) {
+        alert("Please make sure numbers total 100");
+        arr[4].value = null;
+        return false;
+    }
+    return true;
+}
 
 class Home extends React.Component {
+
     constructor(props) {
         super(props);
-        this.state = {
-            vendor:'',
-            product:'',
-            version:'',
-            exe:'',
-            safety:'',
-            security:'',
-            availability:'',
-            resiliency:'',
-            reliability:''
-        };
+        this.state = {};
         this.onInputChange = this.onInputChange.bind(this);
         this.onSubmitForm = this.onSubmitForm.bind(this);
+        this.setPercentage = this.setPercentage.bind(this);
     }
 
     onInputChange(event) {
@@ -25,18 +46,25 @@ class Home extends React.Component {
             [event.target.name]:event.target.value
         });
     }
-
+    
+    setPercentage(event){
+        this.setState({
+            [event.target.id]:event.target.value
+        });
+    }
     async onSubmitForm(event) {
-        const object = {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(this.state)
-        }
+        if (ifSmallerSum()) {
+            const object = {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(this.state)
+            }
 
-        const response = await fetch('submit', object);
-        await response.json().then(data =>
-            alert("Verdict " + JSON.stringify(data))
-        )
+            const response = await fetch('submit', object);
+            await response.json().then(data =>
+                alert("Verdict " + JSON.stringify(data))
+            )
+        }
         event.preventDefault();
     }
 
@@ -49,61 +77,117 @@ class Home extends React.Component {
                         <div>
                             <form>
                                 <label className={"btn input-group-addon"}> <FontAwesomeIcon icon={faFolder} className={"icon"}/>
-                                    <input type="file" value={this.state.value} onChange={this.onInputChange} accept={".exe"}/>
+                                    <input name={"exe"} type="file" value={this.state.value}
+                                           onChange={this.onInputChange} accept={".exe"}/>
                                     Browse
                                 </label>
-                                <input name={"exe"} className={"file-browse input-group-addon"} type="text" value={this.state.value}
-                                       placeholder={"Choose application"} onChange={this.onInputChange}/>
+                                <input name={"exe"} className={"file-browse input-group-addon"} type="text"
+                                       value={this.state.exe}
+                                       placeholder={"Choose application"}
+                                       onChange={this.onInputChange}/>
                             </form>
 
                             <form>
                                 {/* Vendor field */}
-                                <label className={"field-label input-group-addon"}> Vendor </label>
-                                <input type={"fieldLabel"} value={this.state.value} onChange={this.onInputChange}/>
-                                <input name={"vendor"} className={"text input-group-addon"} type="text" value={this.state.vendor}
+                                <Popup content='Name of the company selling the software'
+                                       trigger={<label className={"field-label input-group-addon"}> Vendor
+                                           <FontAwesomeIcon icon={faQuestionCircle} className={"qmark"}/>
+                                       </label>}
+                                       className={"popup"}
+                                       mouseEnterDelay={1000}
+                                       mouseLeaveDelay={500}/>
+                                <input name={"vendor"} className={"text input-group-addon"} type="text"
+                                       value={this.state.vendor}
                                        placeholder={"Microsoft"} onChange={this.onInputChange}/>
 
                                 {/* Product field */}
-                                <label className={"field-label input-group-addon"}> Product </label>
-                                <input type={"fieldLabel"} value={this.state.value} onChange={this.onInputChange}/>
-                                <input name={"product"} className={"text input-group-addon"} type="text" value={this.state.product}
+                                <Popup content='Name of the software/application to be tested'
+                                       trigger={<label className={"field-label input-group-addon"}> Product
+                                           <FontAwesomeIcon icon={faQuestionCircle} className={"qmark"}/>
+                                       </label>}
+                                       className={"popup"}
+                                       mouseEnterDelay={1000}
+                                       mouseLeaveDelay={500}/>
+                                <input name={"product"} className={"text input-group-addon"} type="text"
+                                       value={this.state.product}
                                        placeholder={"Excel"} onChange={this.onInputChange}/>
 
                                 {/* Version field */}
-                                <label className={"field-label input-group-addon"}> Version number </label>
-                                <input type={"fieldLabel"} value={this.state.value} onChange={this.onInputChange}/>
-                                <input name={"version"} className={"text input-group-addon"} type="text" value={this.state.version}
+                                <Popup content='The version no. of the application, if known.'
+                                       trigger={<label className={"field-label input-group-addon"}> Version number
+                                           <FontAwesomeIcon icon={faQuestionCircle} className={"qmark"}/>
+                                       </label>}
+                                       className={"popup"}
+                                       mouseEnterDelay={1000}
+                                       mouseLeaveDelay={500}/>
+                                <input name={"version"} className={"text input-group-addon"} type="text"
+                                       value={this.state.version}
                                        placeholder={"2016"} onChange={this.onInputChange}/>
-
                             </form>
 
                             <form>
-
-                                <label className={"field-label input-group-addon"}> Security </label>
-                                <input type={"fieldLabel"} value={this.state.value} onChange={this.onInputChange}/>
-                                <input name={"security"} className={"percentage input-group-addon"} type="number" min={0} max={100} value={this.state.security}
-                                       placeholder={"20"} onChange={this.onInputChange}/>
-
-
-                                <label className={"field-label input-group-addon"}> Safety </label>
-                                <input type={"fieldLabel"} value={this.state.value} onChange={this.onInputChange}/>
-                                <input name={"safety"} className={"percentage input-group-addon"} type="number" min={0} max={100}  value={this.state.safety}
-                                       placeholder={"20"} onChange={this.onInputChange}/>
-
-                                <label className={"field-label input-group-addon"}> Resiliency </label>
-                                <input type={"fieldLabel"} value={this.state.value} onChange={this.onInputChange}/>
-                                <input name={"resiliency"} className={"percentage input-group-addon"} type="number" min={0} max={100} value={this.state.resiliency}
-                                       placeholder={"20"} onChange={this.onInputChange}/>
-
-                                <label className={"field-label input-group-addon"}> Reliability </label>
-                                <input type={"fieldLabel"} value={this.state.value} onChange={this.onInputChange}/>
-                                <input name={"reliability"} className={"percentage input-group-addon"} type="number"min={0} max={100}  value={this.state.reliability}
-                                       placeholder={"20"} onChange={this.onInputChange}/>
-
-                                <label className={"field-label input-group-addon"}> Availability </label>
-                                <input type={"fieldLabel"} value={this.state.value} onChange={this.onInputChange}/>
-                                <input name={"availability"} className={"percentage input-group-addon"} type="number" min={0} max={100} value={this.state.availability}
-                                       placeholder={"20"} onChange={this.onInputChange}/>
+                                <Popup content='Security - The ability of the software to remain protected against the hazards'
+                                       trigger={<label className={"field-label input-group-addon"}> Security
+                                           <FontAwesomeIcon icon={faQuestionCircle} className={"qmark"}/>
+                                       </label>}
+                                       className={"popup"}
+                                       mouseEnterDelay={1000}
+                                       mouseLeaveDelay={500}/>
+                                <input name="percentage" id={"security"} className={"percentage input-group-addon"}
+                                       type="text"
+                                       pattern="[0-9]*" maxLength={3} value={this.state.value}
+                                       placeholder={"20"} onInput={findTotal} onChange={this.setPercentage}/>
+                                       
+                                <Popup content='Safety - The ability of the software to operate without causing harm to anything or anyone'
+                                       trigger={<label className={"field-label input-group-addon"}> Safety<FontAwesomeIcon
+                                           icon={faQuestionCircle}
+                                           className={"qmark"}/> </label>}
+                                       className={"popup"}
+                                       mouseEnterDelay={1000}
+                                       mouseLeaveDelay={500}/>
+                                <input name="percentage" id={"safety"} className={"percentage input-group-addon"}
+                                       type="text"
+                                       pattern="[0-9]*" maxLength={3} value={this.state.value}
+                                       placeholder={"20"} onInput={findTotal} onChange={this.setPercentage}/>
+    
+                                <Popup content='Resilience -The ability of the software to recover from errors quickly and completely.'
+                                       trigger={<label className={"field-label input-group-addon"}> Resiliency <FontAwesomeIcon
+                                           icon={faQuestionCircle}
+                                           className={"qmark"}/></label>}
+                                       className={"popup"}
+                                       mouseEnterDelay={1000}
+                                       mouseLeaveDelay={500}/>
+                                
+                                <input name="percentage" id={"resiliency"} className={"percentage input-group-addon"}
+                                       type="text"
+                                       pattern="[0-9]*" maxLength={3} value={this.state.value}
+                                       placeholder={"20"} onInput={findTotal} onChange={this.setPercentage}/>
+    
+                                <Popup content='Reliability - The ability of the software to operate correctly.'
+                                       trigger={<label className={"field-label input-group-addon"}> Reliability <FontAwesomeIcon
+                                           icon={faQuestionCircle}
+                                           className={"qmark"}/></label>}
+                                       className={"popup"}
+                                       mouseEnterDelay={1000}
+                                       mouseLeaveDelay={500}/>
+                                
+                                <input name="percentage" id={"reliability"} className={"percentage input-group-addon"}
+                                       type="text"
+                                       pattern="[0-9]*" maxLength={3} value={this.state.value}
+                                       placeholder={"20"} onInput={findTotal} onChange={this.setPercentage}/>
+    
+                                <Popup content='Availability - The ability of the software to operate when required.'
+                                       trigger={ <label className={"field-label input-group-addon"}> Availability <FontAwesomeIcon
+                                           icon={faQuestionCircle}
+                                           className={"qmark"}/></label>
+                                       }
+                                       className={"popup"}
+                                       offset={[-230,0]}
+                                       mouseEnterDelay={1000}
+                                       mouseLeaveDelay={500}/>
+                                <input name="percentage" id={"availability"} className={"percentage input-group-addon"}
+                                       type="text" pattern="[0-9]*" maxLength={3} value={this.state.value}
+                                       placeholder={"20"} onInput={findTotal} onChange={this.setPercentage}/>
 
                             </form>
                             <input type={"submit"} className={"btn input-group-addon"} onClick={this.onSubmitForm}/>
