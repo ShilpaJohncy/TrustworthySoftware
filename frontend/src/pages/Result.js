4c6c2534d41b693d1e51725acf2a9dc3984c0b96
Shilpa <shilpamaria.johncy@gmail.com>
2021-03-27 13:29:07 +0000

    Changed the way the 'loading' functionality is shown

--- frontend/src/pages/Result.js
+++ frontend/src/pages/Result.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import { faQuestionCircle} from "@fortawesome/free-solid-svg-icons";
-import {Popup} from "semantic-ui-react";
+import {Button, Popup} from "semantic-ui-react";
 import Loader from 'react-loader-spinner';
 import Home from "./Home";
 
@@ -43,109 +43,117 @@ class Result extends Component {
         if (this.state.redirect) {
             return <Home />
         }
-        return (<div className="contents">
-                <h1>Trustworthy Analyser Verdict</h1>
+        return (
+            <div className="contents">
+                {
+                    (this.state.isFetching) ?
+                        <h1> Analysing product
+                            <Loader type="ThreeDots" color="black" height="25" width="40" />
+                        </h1>:
+                        <h1>Trustworthy Analyser Verdict
+                            <Loader height="0" width="0" />
+                        </h1>
+                }
+
                 <div className="wrapper">
                     <div className="box">
-                        <div> {(this.state.isFetching) ?
-                            <div> <p className={"fetch"}> Fetching results </p>
-                                <Loader type="ThreeDots" color="black"/>
-                            </div> : <div>
-                                <form>
-                                    <Popup content='Security - The ability of the software to remain protected against the hazards'
-                                           trigger={<label className={"field-label input-group-addon"}> Security
-                                               <FontAwesomeIcon icon={faQuestionCircle} className={"qmark"}/>
-                                           </label>}
-                                           className={"popup"}
-                                           mouseEnterDelay={1000}
-                                           mouseLeaveDelay={500}/>
-                                    <input id={"security"} className={"percentage input-group-addon"}
-                                           type="text"
-                                           pattern="[0-9]*" maxLength={3} value={JSON.stringify(this.state.result.securityScore)}
-                                           placeholder={"0"} disabled/>
+                        <div>
+                            <form>
+                                <Popup content='Security - The ability of the software to remain protected against the hazards'
+                                       trigger={<label className={"field-label input-group-addon"}> Security
+                                           <FontAwesomeIcon icon={faQuestionCircle} className={"qmark"}/>
+                                       </label>}
+                                       className={"popup"}
+                                       mouseEnterDelay={1000}
+                                       mouseLeaveDelay={500}/>
+                                <input id={"security"} className={"percentage input-group-addon"}
+                                       type="text"
+                                       pattern="[0-9]*" maxLength={3} value={JSON.stringify(this.state.result.securityScore)}
+                                       placeholder={"0"} disabled/>
 
-                                    <Popup content='Safety - The ability of the software to operate without causing harm to anything or anyone'
-                                           trigger={<label className={"field-label input-group-addon"}> Safety<FontAwesomeIcon
-                                               icon={faQuestionCircle}
-                                               className={"qmark"}/> </label>}
-                                           className={"popup"}
-                                           mouseEnterDelay={1000}
-                                           mouseLeaveDelay={500}/>
-                                    <input name="percentage" id={"safety"} className={"percentage input-group-addon"}
-                                           type="text"
-                                           pattern="[0-9]*" maxLength={3} value={JSON.stringify(this.state.result.safetyScore)}
-                                           placeholder={"0"} disabled/>
+                                <Popup content='Safety - The ability of the software to operate without causing harm to anything or anyone'
+                                       trigger={<label className={"field-label input-group-addon"}> Safety<FontAwesomeIcon
+                                           icon={faQuestionCircle}
+                                           className={"qmark"}/> </label>}
+                                       className={"popup"}
+                                       mouseEnterDelay={1000}
+                                       mouseLeaveDelay={500}/>
+                                <input name="percentage" id={"safety"} className={"percentage input-group-addon"}
+                                       type="text"
+                                       pattern="[0-9]*" maxLength={3} value={JSON.stringify(this.state.result.safetyScore)}
+                                       placeholder={"0"} disabled/>
 
-                                    <Popup content='Resilience -The ability of the software to recover from errors quickly and completely.'
-                                           trigger={<label className={"field-label input-group-addon"}> Resiliency <FontAwesomeIcon
-                                               icon={faQuestionCircle}
-                                               className={"qmark"}/></label>}
-                                           className={"popup"}
-                                           mouseEnterDelay={1000}
-                                           mouseLeaveDelay={500}/>
+                                <Popup content='Resilience -The ability of the software to recover from errors quickly and completely.'
+                                       trigger={<label className={"field-label input-group-addon"}> Resiliency <FontAwesomeIcon
+                                           icon={faQuestionCircle}
+                                           className={"qmark"}/></label>}
+                                       className={"popup"}
+                                       mouseEnterDelay={1000}
+                                       mouseLeaveDelay={500}/>
 
-                                    <input name="percentage" id={"resiliency"} className={"percentage input-group-addon"}
-                                           type="text"
-                                           pattern="[0-9]*" maxLength={3} value={JSON.stringify(this.state.result.resilienceScore)}
-                                           placeholder={"0"} disabled/>
+                                <input name="percentage" id={"resiliency"} className={"percentage input-group-addon"}
+                                       type="text"
+                                       pattern="[0-9]*" maxLength={3} value={JSON.stringify(this.state.result.resilienceScore)}
+                                       placeholder={"0"} disabled/>
 
-                                    <Popup content='Reliability - The ability of the software to operate correctly.'
-                                           trigger={<label className={"field-label input-group-addon"}> Reliability <FontAwesomeIcon
-                                               icon={faQuestionCircle}
-                                               className={"qmark"}/></label>}
-                                           className={"popup"}
-                                           mouseEnterDelay={1000}
-                                           mouseLeaveDelay={500}/>
+                                <Popup content='Reliability - The ability of the software to operate correctly.'
+                                       trigger={<label className={"field-label input-group-addon"}> Reliability <FontAwesomeIcon
+                                           icon={faQuestionCircle}
+                                           className={"qmark"}/></label>}
+                                       className={"popup"}
+                                       mouseEnterDelay={1000}
+                                       mouseLeaveDelay={500}/>
 
-                                    <input name="percentage" id={"reliability"} className={"percentage input-group-addon"}
-                                           type="text"
-                                           pattern="[0-9]*" maxLength={3} value={JSON.stringify(this.state.result.reliabilityScore)}
-                                           placeholder={"0"} disabled/>
+                                <input name="percentage" id={"reliability"} className={"percentage input-group-addon"}
+                                       type="text"
+                                       pattern="[0-9]*" maxLength={3} value={JSON.stringify(this.state.result.reliabilityScore)}
+                                       placeholder={"0"} disabled/>
 
-                                    <Popup content='Availability - The ability of the software to operate when required.'
-                                           trigger={ <label className={"field-label input-group-addon"}> Availability <FontAwesomeIcon
-                                               icon={faQuestionCircle}
-                                               className={"qmark"}/></label>
-                                           }
-                                           className={"popup"}
-                                           offset={[-230,0]}
-                                           mouseEnterDelay={1000}
-                                           mouseLeaveDelay={500}/>
-                                    <input name="percentage" id={"availability"} className={"percentage input-group-addon"}
-                                           type="text" pattern="[0-9]*" maxLength={3} value={JSON.stringify(this.state.result.availabilityScore)}
-                                           placeholder={"0"} disabled/>
+                                <Popup content='Availability - The ability of the software to operate when required.'
+                                       trigger={ <label className={"field-label input-group-addon"}> Availability <FontAwesomeIcon
+                                           icon={faQuestionCircle}
+                                           className={"qmark"}/></label>
+                                       }
+                                       className={"popup"}
+                                       offset={[-230,0]}
+                                       mouseEnterDelay={1000}
+                                       mouseLeaveDelay={500}/>
+                                <input name="percentage" id={"availability"} className={"percentage input-group-addon"}
+                                       type="text" pattern="[0-9]*" maxLength={3} value={JSON.stringify(this.state.result.availabilityScore)}
+                                       placeholder={"0"} disabled/>
 
-                                </form>
-                                <form>
-                                    <Popup content='Trustworthy Score'
-                                           trigger={ <label className={"field-label input-group-addon"}> Trustworthy score <FontAwesomeIcon
-                                               icon={faQuestionCircle}
-                                               className={"qmark"}/></label>
-                                           }
-                                           className={"popup"}
-                                           offset={[-230,0]}
-                                           mouseEnterDelay={1000}
-                                           mouseLeaveDelay={500}/>
-                                    <input name="score" className={"percentage input-group-addon"} type="text"
-                                           pattern="[0-9]*" maxLength={3} value={JSON.stringify(this.state.result.trustworthyScore)}
-                                           placeholder={"0"} disabled/>
+                            </form>
+                            <form>
+                                <Popup content='Trustworthy Score'
+                                       trigger={ <label className={"field-label input-group-addon"}> Trustworthy score <FontAwesomeIcon
+                                           icon={faQuestionCircle}
+                                           className={"qmark"}/></label>
+                                       }
+                                       className={"popup"}
+                                       offset={[0,0]}
+                                       mouseEnterDelay={1000}
+                                       mouseLeaveDelay={500}/>
+                                <input name="score" className={"percentage input-group-addon"} type="text"
+                                       pattern="[0-9]*" maxLength={3} value={JSON.stringify(this.state.result.trustworthyScore)}
+                                       placeholder={"0"} disabled/>
 
-                                </form>
-                                <form>
-                                    <Popup content='Trustworthy Verdict'
-                                           trigger={<label className={"field-label input-group-addon"}> Verdict
-                                               <FontAwesomeIcon icon={faQuestionCircle} className={"qmark"}/>
-                                           </label>}
-                                           className={"popup"}
-                                           mouseEnterDelay={1000}
-                                           mouseLeaveDelay={500}/>
-                                    <input id={"verdict"} className={"verdict input-group-addon"}
-                                           type="text" value={(this.state.result.verdict)} disabled/>
-                                </form>
-                                <input value={"Run another"}type={"submit"} className={"btn input-group-addon"} onClick={this.onReturn}/>
-
-                            </div>
-                        }
+                            </form>
+                            <form>
+                                <Popup content='Trustworthy Verdict'
+                                       trigger={<label className={"field-label input-group-addon"}> Verdict
+                                           <FontAwesomeIcon icon={faQuestionCircle} className={"qmark"}/>
+                                       </label>}
+                                       className={"popup"}
+                                       mouseEnterDelay={1000}
+                                       mouseLeaveDelay={500}/>
+                                <input id={"verdict"} className={"verdict input-group-addon"}
+                                       type="text" placeholder={"Verdict"} value={(this.state.result.verdict)} disabled/>
+                            </form>
+                            <Button disabled={this.state.isFetching}
+                                    style={!this.state.isFetching ? {opacity: 1} : {opacity: 0.5}}
+                                    className={"btn input-group-addon"} onClick={this.onReturn}>
+                                Run another
+                            </Button>
                         </div>
                     </div>
                 </div>
